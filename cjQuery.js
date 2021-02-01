1969e9e111927de920ab6107fb0090a417e713f4
Chanjin Park <inf0craw1@naver.com>
2021-02-01 18:30:01 +0900

    on eventhandler added

--- cjQuery.js
+++ cjQuery.js
@@ -1,34 +1,50 @@
 function $(s){
-    $.prototype.selector = document.querySelectorAll(s);
-    return $.prototype;
-}
-$.prototype = {
-    selector: null,
-    color: function(c){
-        for(elem of this.selector){
-            elem.style.color = c;
+    $.prototype = {
+        selector: document.querySelectorAll(s),
+        selectString: s,
+        color: function(c){
+            for(elem of this.selector){
+                elem.style.color = c;
+            }
+            return this;
+        },
+        text: function(t){
+            for(elem of this.selector){
+                elem.innerHTML = t;
+            }
+            return this;
+        },
+        hide: function(){
+            for(elem of this.selector){
+                elem.style.display = "none";
+            }
+            return this;
+        },
+        show: function(){
+            for(elem of this.selector){
+                elem.style.display = "block";
+            }
+            return this;
+        },
+        on: function(e, f){
+            for(elem of this.selector){
+                elem.addEventListener(e, f);
+            }
+            return this;
+        },
+        child: function(e){
+            console.log(this.selector);
+            console.log(this.selector.childNodes);
+            // let s = `${this.selectString} ${e}`;
+            // this.selector = document.querySelectorAll(s);
+            // this.selectString = s;
+            return this;
+        },
+        sibling: function(e){
+            console.log(this.selector);
+            console.log(this.selctString);
+            return this;
         }
-        return this.prototype;
-    },
-    text: function(t){
-        for(elem of this.selector){
-            elem.innerHTML = t;
-        }
-        return this.prototype;
-    },
-    hide: function(){
-        for(elem of this.selector){
-            elem.style.display = "none";
-        }
-        return this.prototype;
-    },
-    show: function(){
-        for(elem of this.selector){
-            elem.style.display = "block";
-        }
-        return this.prototype;
-    },
-    on: function(e, f){
-
     }
+    return $.prototype;
 }
