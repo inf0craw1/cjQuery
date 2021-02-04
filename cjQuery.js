90914b88c83b0317cb1d0a5651daca700331ce52
Chanjin Park <inf0craw1@naver.com>
2021-02-04 14:47:29 +0900

    selector validation added

--- cjQuery.js
+++ cjQuery.js
@@ -6,16 +6,31 @@
  */
 
 function cjQuery(s){
+    function isSelectorExist(selector){
+        if(selector[0] != undefined){
+            return true;
+        }
+        return false;
+    }
+    function validateSelector(selector){
+        if(!isSelectorExist(selector)){
+            console.warn(`Element "${s}" is not exist`); return false;
+        }
+        return true;
+    }
+
     cjQuery.prototype = {
         selector: (typeof s === "string") ? document.querySelectorAll(s) : [s],
         selectString: s,
         color: function(c){
+            if(!validateSelector(this.selector)) return false;
             for(elem of this.selector){
                 elem.style.color = c;
             }
             return this;
         },
         text: function(t){
+            if(!validateSelector(this.selector)) return false;
             if(t === null || t === undefined){
                 if(this.selector.length > 1){
                     let res = [];
@@ -32,18 +47,21 @@ function cjQuery(s){
             return this;
         },
         hide: function(){
+            if(!validateSelector(this.selector)) return false;
             for(elem of this.selector){
                 elem.style.display = "none";
             }
             return this;
         },
         show: function(){
+            if(!validateSelector(this.selector)) return false;
             for(elem of this.selector){
                 elem.style.display = "block";
             }
             return this;
         },
         value: function(v){
+            if(!validateSelector(this.selector)) return false;
             if(v === null || v === undefined){
                 if(this.selector.length > 1){
                     let res = [];
@@ -60,6 +78,7 @@ function cjQuery(s){
             return this;
         },
         css: function(prop, propValue){
+            if(!validateSelector(this.selector)) return false;
             if(propValue === null || propValue === undefined){
                 if(this.selector.length > 1){
                     let res = [];
@@ -76,12 +95,14 @@ function cjQuery(s){
             return this;
         },
         on: function(e, f){
+            if(!validateSelector(this.selector)) return false;
             for(elem of this.selector){
                 elem.addEventListener(e, f);
             }
             return this;
         },
         attr: function(attrName, attrValue){
+            if(!validateSelector(this.selector)) return false;
             if(attrValue === null || attrValue === undefined){
                 if(this.selector.length > 1){
                     let res = [];
@@ -98,28 +119,33 @@ function cjQuery(s){
             return this;
         },
         parent: function(){
+            if(!validateSelector(this.selector)) return false;
             this.selector = this.selector[0].parentNode;
             return this;
         },
         addClass: function(c) {
+            if(!validateSelector(this.selector)) return false;
             for(elem of this.selector){
                 elem.classList.add(c);
             }
             return this;
         },
         removeClass: function(c){
+            if(!validateSelector(this.selector)) return false;
             for(elem of this.selector){
                 elem.classList.remove(c);
             }
             return this;
         },
         toggleClass: function(c){
+            if(!validateSelector(this.selector)) return false;
             for(elem of this.selector){
                 elem.classList.toggle(c);
             }
             return this;
         },
         hasClass: function(c){
+            if(!validateSelector(this.selector)) return false;
             if(this.selector.length > 1){
                 let res = [];
                 for(elem of this.selector){
@@ -130,6 +156,7 @@ function cjQuery(s){
             return this.selector[0].classList.contains(c);
         },
         replaceClass: function(targetC, c){
+            if(!validateSelector(this.selector)) return false;
             for(elem of this.selector){
                 elem.classList.replace(targetC, c);
             }
